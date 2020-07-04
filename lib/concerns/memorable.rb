84103824aa248b2a73fde0f4766e95dbfdb57025
Arif Cagirici <acagirici23@gmail.com>
2020-07-04 05:40:41 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,12 +1,10 @@
 module Memorable
-  module ResetAll
     def self.reset_all
-    self.all.clear
+      self.all.clear
     end
-  end
-  module Count
+  
     def self.count
       self.all.count
     end
-  end
+  
 end
